89c24921f89b0672aa2daaf4402d97e29e898bf4
chirag1702 <chirag.jangid1702@gmail.com>
2020-09-17 17:11:46 +0530

    .

--- router.js
+++ router.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
-const parse = require('querystring');
+// const parse = require('querystring');
 const mongoose = require("mongoose");
 const models = require("./models");
-const fs = require("file-system");
-const multer = require("multer");
-const { count, info } = require("console");
+// const fs = require("file-system");
+// const multer = require("multer");
+// const { count, info } = require("console");
 const formidable = require("formidable");
-const multiparty = require("multiparty");
+// const multiparty = require("multiparty");
 const url = require("url");
+const util = require('util');
 
 
 mongoose.connect("mongodb://localhost:27017/ekart_DB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).then(console.log("Connected!!"));
@@ -1124,24 +1125,48 @@ router.post("/api-firebase/sections", (req, res) => {
     var urlParsed = url.parse(req.url, true);
     var urlQuery = urlParsed.query;
     var getVal = urlQuery.getallsections;
-    var productArray;
-    var variantsArray;
+    var productArray = [];
+    var productObject;
+    var variantsArray = [];
+    var variantsObject;
+    var sectionArray = [];
+    var sectionObject;
     models.product.find({}, (err, productResult) => {
         if (err) {
             console.log(err);
         }
 
         else {
-            console.log(productResult);
+            
             for (let index = 0; index < productResult.length; index++) {
-                models.productVariant.findById({id:productResult[index].id}, (err2, variantsResult) => {
+                models.productVariant.findOne({}, (err2, variantsResult) => {
                     if (err2) {
                         console.log(err2);
                     }
 
-                    
+                    else {
+                        if (variantsResult != null) {
+                            for (let i = 0; i < variantsResult.length; i++) {
+                                variantsObject = {
+                                    "id": variantsResult[i].id,
+                                    "product_id": variantsResult[i].product_id,
+                                    "type": variantsResult[i].type,
+                                    "measurment": variantsArray[i].measurment,
+                                    "measurment_unit_id": variantsArray[i].measurment_unit_id,
+                                    "price": variantsArray[i].price,
+                                    "discounted_price": variantsArray[i].discounted_price,
+                                    "serve_for": variantsArray[i].serve_for,
+                                    "stock": variantsArray[i].stock,
+                                    "stock_unit_id": variantsArray[i].stock_unit_id,
+                                    "measurment_unit_name": variantsArray[i].measurment_unit_name,
+                                    "stock_unit_name": variantsArray[i].stock_unit_name
+                                };
+                                variantsArray.push(variantsObject);
+                            }
+                        }
+                    }
                 });
-                productArray = {
+                productObject = {
                     "id": productResult[index].id,
                     "name": productResult[index].name,
                     "slug": productResult[index].slug,
@@ -1155,7 +1180,29 @@ router.post("/api-firebase/sections", (req, res) => {
                     "date_added": productResult[index].date_added,
                     "variants": variantsArray
                 };
+                productArray.push(productObject);
             }
+            
+            models.section.find({}, (err3, sectionsResult) => {
+                if (err3) {
+                    console.log(err3);
+                }
+
+                else {
+                    for (let j = 0; j < sectionsResult.length; j++) {
+                        sectionObject = {
+                            "id": sectionsResult[j].id,
+                            "title": sectionsResult[j].title,
+                            "short_desc": sectionsResult[j].short_description,
+                            "product_ids": sectionsResult[j].product_ids,
+                            "products": productArray
+                        };
+                        sectionArray.push(sectionObject);
+                    }
+                    res.send(sectionArray);
+                }
+            });
+
         }
     });
 });
